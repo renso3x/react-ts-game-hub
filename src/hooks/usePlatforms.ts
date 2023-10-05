9299fe8fee3a029a2730d268a60acb6b250fbe7c
Romeo Enso <romeo.enso@connexionltd.com>
2023-10-06 10:38:21 +1100

    remove duplicate interface

--- src/hooks/usePlatforms.ts
+++ src/hooks/usePlatforms.ts
@@ -1,8 +1,12 @@
-import { Platform } from "./useGames";
 import { useQuery } from "@tanstack/react-query";
 import { CACHE_KEY_PLATFORMS } from "../constants";
 import apiClient, { FetchResponse } from "../services/api-client";
 
+export interface Platform {
+  id: number;
+  name: string;
+  slug: string;
+}
 const usePlatforms = () =>
   useQuery({
     queryKey: CACHE_KEY_PLATFORMS,
